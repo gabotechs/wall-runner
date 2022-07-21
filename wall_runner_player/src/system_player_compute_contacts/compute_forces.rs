e5e27ffedadd08e932326e170b46ef998dcaf2b2
Automated <Automated>
2022-07-21 22:40:59 +0200

    refactor: better system management

---
+++ wall_runner_player/src/system_player_compute_contacts/compute_forces.rs
@@ -0,0 +1,53 @@
+use crate::{Player, PlayerSettings, PlayerState};
+use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+
+#[derive(Default)]
+pub struct PlayerForces {
+    pub has_vertical_forces: bool,
+    pub horizontal_force: Option<Vec3>,
+}
+
+pub(crate) fn compute_forces(
+    rapier_context: &RapierContext,
+    settings: &PlayerSettings,
+    player_state: &mut PlayerState,
+    player_query: &mut Query<Entity, With<Player>>,
+) -> PlayerForces {
+    let mut normal_forces: Vec<Vec3> = vec![];
+    for entity in player_query.iter_mut() {
+        for contact_pair in rapier_context.contacts_with(entity) {
+            let direction = if contact_pair.collider1() == entity {
+                -1.0
+            } else {
+                1.0
+            };
+            let best_manifold = contact_pair.find_deepest_contact();
+            if let Some((manifold, _)) = best_manifold {
+                normal_forces.push(manifold.normal() * direction);
+            }
+        }
+        player_state.something_above = rapier_context
+            .cast_shape(
+                player_state.position,
+                Rot::default(),
+                Vec3::Y,
+                &Collider::ball(settings.width),
+                (0.5 + 0.25) * settings.height,
+                InteractionGroups::all(),
+                Some(&|e| e != entity),
+            )
+            .is_some();
+    }
+    // if the manifold is positive and vertical, then we are on the ground
+    let mut player_forces = PlayerForces::default();
+    for normal_force in normal_forces {
+        if normal_force.y > 0.9 {
+            player_forces.has_vertical_forces = true;
+        }
+        if (normal_force.x.powi(2) + normal_force.z.powi(2)).sqrt() > 0.9 {
+            player_forces.horizontal_force = Some(normal_force)
+        }
+    }
+    player_forces
+}
---
+++ wall_runner_player/src/system_player_compute_contacts/compute_in_ground.rs
@@ -0,0 +1,33 @@
+use crate::system_player_compute_contacts::compute_forces::PlayerForces;
+use crate::{PlayerSettings, PlayerState};
+use bevy::prelude::*;
+
+pub fn compute_in_ground(
+    player_forces: &PlayerForces,
+    settings: &PlayerSettings,
+    player_state: &mut PlayerState,
+) {
+    if player_forces.has_vertical_forces && !player_state.is_in_ground {
+        if player_state.ground_vote < settings.ground_votes {
+            player_state.ground_vote += settings.ground_up_vote;
+            debug!("[+] vote ground {}", player_state.ground_vote);
+        }
+        if player_state.ground_vote >= settings.ground_votes {
+            info!("in ground");
+            player_state.is_in_ground = true;
+        }
+    } else if player_forces.has_vertical_forces && player_state.is_in_ground {
+        player_state.ground_vote = settings.ground_votes;
+    } else if !player_forces.has_vertical_forces && player_state.is_in_ground {
+        if player_state.ground_vote > 0 {
+            player_state.ground_vote -= settings.ground_down_vote;
+            debug!("[-] vote ground {}", player_state.ground_vote);
+        }
+        if player_state.ground_vote == 0 {
+            info!("not in ground");
+            player_state.is_in_ground = false;
+        }
+    } else if !player_forces.has_vertical_forces && !player_state.is_in_ground {
+        player_state.ground_vote = 0;
+    }
+}
